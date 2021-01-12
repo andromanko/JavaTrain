e3b9897a6864256468342f7351a1d2c0f0bdc8b1
roma <roma@roma-i3>
2021-01-12 22:06:05 +0300

    epam tests solved without treenode

--- EPM/src/epm/Tsk0.java
+++ EPM/src/epm/Tsk0.java
@@ -14,23 +14,29 @@ public class Tsk0 {
 	//     System.out.println(reverseString("asdfghjkl"));
 	  //   System.out.println(reverseString("adf"));
 	//     System.out.println(reverseString(null));
-	    System.out.println("isPrime:"+isPrime(12)+" "+isPrime(13)+" "+isPrime(2)+" "+isPrime(42));
-	     System.out.println("toggleDoors:"+toggleDoors(1)+"\n_2_"+toggleDoors(2)+"\n_4_"+toggleDoors(4)+"\n_9:_"+toggleDoors(9)+"\n___");
-	     int arr[]= {5,3,4,6,7,8,4,3,7,9}//{1,4,6,2,5,9,5,6,8,2}
+	    //System.out.println("isPrime:"+isPrime(12)+" "+isPrime(13)+" "+isPrime(2)+" "+isPrime(42));
+	     //System.out.println("toggleDoors:"+toggleDoors(1)+"\n_2_"+toggleDoors(2)+"\n_4_"+toggleDoors(4)+"\n_9:_"+toggleDoors(9)+"\n___");
+	 /*    int arr[]= {5,3,4,6,7,8,4,3,7,9}//{1,4,6,2,5,9,5,6,8,2}
 	     	,arr1[]= {164,67,55,4,6,476,378},arr2[]= {64,67,55,94,6,476,378},arr3[]= {4,6,8,10,12,13},arr4[]= {},//;//= {4,4,4,4};;
 	    	arrT1[]= {1,1,1,2,2},arrT2[]= {1,2,5,8,10,12},arrT3[]={1,2,4,7,11,18,29,47},// {10,21,22,100,101,200,300}, //,arrT2[]= ,arrT3[]= ,
 	    	arrM1[]= {1,2,3,4,6,7,8,9},arrM2[]= {2,6,7,3,5,8,9,1},arrM3[]= {1,2,3,4,5,6,7,10},
 	    	arrP1[]= {1,2,3,4,5},arrP2[]= {2,6,4,8},arrP3[]={-4,-2,0,2,4},
 	    	arrSI1[]= {1,3,5,6},arrSI2[]= {},
 	     	arrS1[]= {1,2,3,4,1,1,3},arrS2[]= {4,3,2,1},arrS3[]= {6,6,6,6},arrS4[]= {5,5,5,5},arrS5[]= {1,2,3,4},
-	     	arrEQ1[]= {1,2,3,4,5,6,7,8,9,-45,100},arrEQ2[]= {-1,3,-4,5,1,-6,2,1},arrEQ3[]= {1,3,1},arrEQ4[]= {1,2},arrEQ5[]= {5},arrEQ6[]= {};
-	       
+	     	arrEQ1[]= {1,2,3,4,5,6,7,8,9,-45,100},arrEQ2[]= {-1,3,-4,5,1,-6,2,1},arrEQ3[]= {1,3,1},arrEQ4[]= {1,2},arrEQ5[]= {5},arrEQ6[]= {}
+	 */       ;
+	     Integer arrI1[]= {}//{2,3,5,6,9,12,14,20}
+	     , arrI2[]= {2}//8,9,10,12,32,33};
 	     ;
-	     int arrSym1[][]= {{3,1,1},{1,6,1},{1,1,9}};
+	     int SHV1[]= {1,2,3}, SHV2[]= {1,3,2}, SHV3[]= {-214783648,-214783648};
+	     //int arrSym1[][]= {{3,1,1},{1,6,1},{1,1,9}};
+	   LinkedList<String> strr = new LinkedList<String>();//= new List();//("qq","qwe");
+	   
+	   strr.add("qwe fsga");strr.add("asd asdd");strr.add("as d a");strr.add("qwe");strr.add("qwe er sdfgas");
 	     System.out.println(
 	    //		"PLNDRM:"+isPalindrome("aba")+" "+isPalindrome("abbc")+"\n"+
-	    		 "NumberDeficiency 1: "+NumberDeficiency(1)+" 21:"+NumberDeficiency(21)+" 10:"+NumberDeficiency(10)+" 16: "+NumberDeficiency(16)//+"20: "+NumberDeficiency(20)
-	    		 +"\n"+
+	  //  		 "NumberDeficiency 1: "+NumberDeficiency(1)+" 21:"+NumberDeficiency(21)+" 10:"+NumberDeficiency(10)+" 16: "+NumberDeficiency(16)//+"20: "+NumberDeficiency(20)
+	  //  		 +"\n"+
 	    //	 "reverseNumber:"+reverseNumber(-541)+" "+reverseNumber(-33)+" "+reverseNumber(0)+" "+reverseNumber(55)+" "+reverseNumber(122)+" "+reverseNumber(59841)+"\n"+
 	    //		 "Benefi: "+ getMaxBenefit(arr)+"\n"+
 	     //"narciss:0"+isNarcissistic(0)+" 5: "+isNarcissistic(5)+"10:"+isNarcissistic(10)+" 153: "+isNarcissistic(153)+"8208: "+isNarcissistic(8208)+" "+isNarcissistic(10101)+"\n"+
@@ -38,22 +44,26 @@ public class Tsk0 {
 	     //"isSemiPrime:"+ isSemiPrime(3)+" "+ isSemiPrime(4)+" "+isSemiPrime(8)+" "+ isSemiPrime(26)+"\n "+
 	     //"MinInArr:"+ searchMinValue(arr2)+"\n "+
 	     //"NumBerInArr:"+ isNumberInArray(arr,54)+"\n "+
-	     "countTriangleTriplets:"+ countTriangleTriplets(arrT1)+" "+countTriangleTriplets(arrT2)+" "+countTriangleTriplets(arrT3)+"\n:"+
-	     "findMissingNumber:"+findMissingNumber(arrM1,9)+" "+findMissingNumber(arrM2,9)+" "+findMissingNumber(arrM3,9)+"\n"
+	  //   "countTriangleTriplets:"+ countTriangleTriplets(arrT1)+" "+countTriangleTriplets(arrT2)+" "+countTriangleTriplets(arrT3)+"\n:"+
+	  //   "findMissingNumber:"+findMissingNumber(arrM1,9)+" "+findMissingNumber(arrM2,9)+" "+findMissingNumber(arrM3,9)+"\n"
 	     //"reverseWord:"+reverseWords("the skY is Blue")+"\n"+reverseWords("the cake is a lie")+"\n"+reverseWords(null)+"\nnUll"+reverseWords("")+"\nEmpty\n"+
 	     //"StrParenthesis:"+isValid("2*((5-2)/3)")+" "+isValid("(5-1)*2)")+" "+isValid("()")+" "+isValid("()()")+" "+isValid("((")+" "+isValid("))")+" "+isValid(")(")+" "+isValid(null)+"\n"+
 	     //"isArithmeticProgression:"+ isArithmeticProgression(arrP1)+" "+isArithmeticProgression(arrP2)+" "+isArithmeticProgression(arrP3)+" "+"\n"
-	    + "perfect 6:"+isPerfectNumber(6)+" perfect 8:"+isPerfectNumber(8)+" perfect -1:"+isPerfectNumber(-1)+
-	  "\n"+getLengthOfLongestCommonSunstring("","")+getLengthOfLongestCommonSunstring("nematode knowledge","empty bottle")+
-	  "\n"+ searchInsert(arrSI1,5)+" "+searchInsert(null,2)+" "+searchInsert(arrSI2,2)+" "+searchInsert(arrSI1,7)+" "+searchInsert(arrSI1,0)+
-	  "\n"+solution(4,arrS1)+" "+solution(11,arrS2)+" "+solution(6,arrS3)+" "+solution(6,arrS4)+" "+solution(10,arrS5)
-	  + "\n"+equilibrium(arrEQ1)+" "+equilibrium(arrEQ2)+" "+equilibrium(arrEQ3)+" "+equilibrium(arrEQ4)+" "+equilibrium(arrEQ5)+" "+equilibrium(arrEQ6)+" "
-	   +"\n"+isPolyDivisibleNumber(7)+" "+isPolyDivisibleNumber(12)+" "+isPolyDivisibleNumber(11)+" "+isPolyDivisibleNumber(987654564)+" "+isPolyDivisibleNumber(1111111111)+" "
-	   +"\n"+ chocolatesByNumber(7,4)+" "+ chocolatesByNumber(20,15)+" "+ chocolatesByNumber(10,4)+" "+ chocolatesByNumber(4,10)+" "+ chocolatesByNumber(1000526,72)+
-	   "\n"+isSymmetric(arrSym1));
+	  //  + "perfect 6:"+isPerfectNumber(6)+" perfect 8:"+isPerfectNumber(8)+" perfect -1:"+isPerfectNumber(-1)+
+	//  "\n"+getLengthOfLongestCommonSunstring("","")+getLengthOfLongestCommonSunstring("nematode knowledge","empty bottle")+
+	//  "\n"+ searchInsert(arrSI1,5)+" "+searchInsert(null,2)+" "+searchInsert(arrSI2,2)+" "+searchInsert(arrSI1,7)+" "+searchInsert(arrSI1,0)+
+	//  "\n"+solution(4,arrS1)+" "+solution(11,arrS2)+" "+solution(6,arrS3)+" "+solution(6,arrS4)+" "+solution(10,arrS5)
+	//  + "\n"+equilibrium(arrEQ1)+" "+equilibrium(arrEQ2)+" "+equilibrium(arrEQ3)+" "+equilibrium(arrEQ4)+" "+equilibrium(arrEQ5)+" "+equilibrium(arrEQ6)+" "
+	  // +"\n"+isPolyDivisibleNumber(7)+" "+isPolyDivisibleNumber(12)+" "+isPolyDivisibleNumber(11)+" "+isPolyDivisibleNumber(987654564)+" "+isPolyDivisibleNumber(1111111111)+" "
+	   //+"\n"+ chocolatesByNumber(7,4)+" "+ chocolatesByNumber(20,15)+" "+ chocolatesByNumber(10,4)+" "+ chocolatesByNumber(4,10)+" "+ chocolatesByNumber(1000526,72)+
+	   //"\n"+isSymmetric(arrSym1)
+	   //+"\n longest: "+longestLines(strr)
+	   "\n ads"+getOrderedEvenNumberListFrom2OrderedList(arrI1,arrI2)
+	   //+"\n SSS: "+getEvenNumbersAfterEachIndex("121212"));
 	  //+++ printPascalTriangle(4);printPascalTriangle(3);printPascalTriangle(2);printPascalTriangle(1);
-	    		 
-	    		 
+	    	+"\n 2ndHighest"+secondHighest(SHV1)+" "+secondHighest(SHV2)+" "+secondHighest(SHV3)+" "
+	    	);	 
+	    	
 	    	
 }
 /*	public String practicePuzzle(Candidate candidate, Tutorial tutorial) {
@@ -551,7 +561,7 @@ public static boolean isSymmetric (final int[][] matrix) {
      }
      return retval;
 }
-//not checked
+//++
 public static List<String> longestLines(List<String> inputLines) {
 	if (inputLines==null) {
 		throw new IllegalArgumentException("NOOOT rule argument!!!");
@@ -569,6 +579,71 @@ public static List<String> longestLines(List<String> inputLines) {
 		}
 	}
 	return lines;
+}
+//+
+public static List<Integer> getOrderedEvenNumberListFrom2OrderedList(Integer[] first, Integer[] second) {
+	List<Integer> result = null;
+	if (first !=null && second !=null) {
+		result = new ArrayList<Integer>();
+		int indexOfFirst=0;
+		int indexOfSecond=0;
+		while (indexOfFirst<first.length || indexOfSecond<second.length) {
+			boolean firstAvailable=indexOfFirst<first.length, secondAvailable=indexOfSecond<second.length;Integer currentExaminedNumber=null;
+			if (firstAvailable && secondAvailable && first[indexOfFirst]==second[indexOfSecond]) {
+				currentExaminedNumber=first[indexOfFirst];
+				indexOfFirst++; indexOfSecond++;
+			} else if (firstAvailable && (!secondAvailable || (secondAvailable && first[indexOfFirst]<second[indexOfSecond]))) {
+				currentExaminedNumber=first[indexOfFirst];
+				indexOfFirst++;
+			} else if (secondAvailable && (!firstAvailable ||(firstAvailable && second[indexOfSecond]<first[indexOfFirst]))) {
+				currentExaminedNumber=second[indexOfSecond];
+				indexOfSecond++;
+			}
+			if (currentExaminedNumber !=null && currentExaminedNumber % 2==0) {
+				result.add(currentExaminedNumber);
+			} 
+		}
+	}
+	return result;
+}
+//+
+public static int[] getEvenNumbersAfterEachIndex(String inputString) {
+	if (null == inputString) {
+		throw new IllegalArgumentException("iStr NOT NULL BUST BE");
+	}
+	if (!inputString.matches("[0-9]+")) {
+		throw new IllegalArgumentException("iSot must contani only 0-9 chrs");
+	}
+	char [] characters = inputString.toCharArray();
+	int[] evenNumbersAfterIndex =new int[characters.length] ;
+	for (int i=0;i<characters.length;i++) {
+		for (int j=(i+1);j<characters.length;j++) {	
+			int numericValue=Integer.parseInt(""+characters[j]);
+			if (numericValue % 2==0) {
+				evenNumbersAfterIndex[i]++;
+				
+			}
+		}
+		System.out.println(evenNumbersAfterIndex[i]);
+	}
+	return evenNumbersAfterIndex;
+}
+//+
+public static Integer secondHighest( int[] nums ) {
+	System.out.println(Integer.valueOf(0+"0"));
+	if (nums ==null || nums.length<2) return null;
+	Integer highest=null;
+	Integer secondHighest=null;
+	for (int num: nums) {
+		if (highest==null || num > highest) {
+			secondHighest = highest;
+			highest = num;
+			
+		} else if ((secondHighest==null || num> secondHighest) && num !=highest) {
+	    	 secondHighest=num;
+	     }
+	}
+	return secondHighest;
 }
 
 }
