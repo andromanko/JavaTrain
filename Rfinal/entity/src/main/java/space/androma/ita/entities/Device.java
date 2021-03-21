9057f0ae221aa35f0bccef0ba090b37fa222bd52
roma <roma@roma-i3>
2021-03-22 01:16:20 +0300

    sdfzb

---
+++ Rfinal/entity/src/main/java/space/androma/ita/entities/Device.java
@@ -0,0 +1,38 @@
+package space.androma.ita.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+//import lombok.extern.slf4j.Slf4j;
+
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@SuperBuilder 
+@Entity
+@Table(name="device")
+public class Device extends AEntity<Integer> {
+	
+	@Column(name="IP")
+	private String ip;
+	
+	@Column(name="addr")
+	private byte addr;
+	
+	@Column(name="driverId")
+	private int driverId;
+	
+//	OneToMany(mappedBy="user", fetch=FetchType.LAZY, cascade =CascadeType.PERSIST, orphanRemoval=true)
+//	private List<Events> pets;
+
+}
+
